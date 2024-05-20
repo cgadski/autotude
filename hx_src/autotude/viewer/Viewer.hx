b1cee3b111adb55bc7d2146321bfb2c8571c0b05
Christopher Gadzinski <me@cgad.ski>
2024-05-20 12:29:18 +0200

    style fixes

--- hx_src/autotude/viewer/Viewer.hx
+++ hx_src/autotude/viewer/Viewer.hx
@@ -59,7 +59,7 @@ class Viewer extends hxd.App {
 		draggable.onWheel = onWheel;
 
 		// load map
-		final request = Browser.window.fetch("recordings/zz_hates_whale.pb.gz");
+		final request = Browser.window.fetch("recordings/illum.pb.gz");
 		request.then((res) -> {
 			res.arrayBuffer().then((buf) -> {
 				final replay = new Replay(new BytesInput(Bytes.ofData(buf)));
