80d12c79bd2746b56c4242e5ba3b4df09bd86be7
Christopher Gadzinski <me@cgad.ski>
2024-05-26 15:43:42 +0200

    add hxcpp that works on darwin

--- nix/haxe-packages.nix
+++ nix/haxe-packages.nix
@@ -1,5 +1,7 @@
 {
   stdenv,
+  darwin,
+  xcbuild,
   lib,
   fetchzip,
   fetchFromGitHub,
@@ -52,7 +54,7 @@
             runHook postInstall
           '';
       });
-in {
+in rec {
   inherit buildHaxeLib;
 
   protohx = buildHaxeLib rec {
@@ -85,6 +87,15 @@ in {
       hash = "sha256-+Uue7pdkl67hRq6RXdao/ayFR75ExfoDDIhSB9EOQM0=";
     };
 
+    propagatedBuildInputs = [
+      darwin.apple_sdk.frameworks.Cocoa
+      xcbuild 
+    ];
+
+    patches = [
+      ./hxcpp.diff
+    ];
+
     buildPhase = ''
       (cd tools/hxcpp; haxe compile.hxml)
     '';
@@ -92,7 +103,7 @@ in {
     postFixup = ''
       for f in $out/lib/haxe/${withCommas libname}/${withCommas version}/{,project/libs/nekoapi/}bin/Linux{,64}/*; do
         chmod +w "$f"
-        patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker)   "$f" || true
+        patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) "$f" || true
         patchelf --set-rpath ${lib.makeLibraryPath [stdenv.cc.cc]}  "$f" || true
       done
     '';
@@ -113,14 +124,76 @@ in {
     meta.description = "A Haxe Library for supporting different file formats";
   };
 
-  hxmustache = buildHaxeLib {
+  tokentree = buildHaxeLib rec {
+    libname = "tokentree";
+    version = "1.2.11";
     src = fetchFromGitHub {
-      owner = "nadako";
-      repo = "hxmustache";
-      rev = "756b3880da5035a18909ac4865adc7615c862f79";
-      hash = "sha256-r1BhW+DrrUp0nAyZQgSuBbIEoiSRjFTLiE16c4AlfRM=";
+      owner = "HaxeCheckstyle";
+      repo = "tokentree";
+      rev = "6032e8071073fa01449f51acfdc02a1a610d35e0";
+      hash = "sha256-lVdu9Jhs4NT1w5S5C1OgMmj07DikxX2qO+vtu3DyxaY=";
     };
-    libname = "hxmustache";
-    version = "0.2.2";
+  };
+
+  haxeparser = buildHaxeLib rec {
+    libname = "haxeparser";
+    version = "4.3.0";
+    src = fetchFromGitHub {
+      owner = "HaxeCheckstyle";
+      repo = "haxeparser";
+      rev = "a5fce2ecf5fb3bdfebfd7efd8b05329d456ec0d2";
+      hash = "sha256-voQZT9BJRqlqyc8eFQyjAsr/LBgkY2NZJfFzpkMx/14=";
+    };
+  };
+
+  hxparse = buildHaxeLib rec {
+    libname = "hxparse";
+    version = "4.3.0";
+    sha256 = "sha256-3r351fSXC1AY9zOhZU1SEKyovxSEiay0B/Si7TP+tG4=";
+  };
+
+  json2object = buildHaxeLib rec {
+    libname = "json2object";
+    version = "3.11.0";
+    sha256 = "sha256-M2iJZjI9Un6QOIWd/7Qv0XsQSntq3JJmXwTIzBSEXCo=";
+  };
+
+  hxargs = buildHaxeLib rec {
+    libname = "hxargs";
+    version = "4.0.0";
+    sha256 = "sha256-RIy7Mai2MsmjC0A6F0sCeQu7XQaRjUYiHxVpgTrUSnY=";
+  };
+
+  hxjsonast = buildHaxeLib rec {
+    libname = "hxjsonast";
+    version = "1.1.0";
+    sha256 = "sha256-5Kbq/hDKypx29omnU8bFfd634KqBVYybEmUZh13qjYc=";
+  };
+
+  formatter = stdenv.mkDerivation {
+    name = "formatter";
+
+    buildPhase = ''
+      haxe buildCpp.hxml
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin/
+      cp out/formatter $out/bin/hx-fmt
+    '';
+
+    HXCPP_CLANG = true;
+
+    buildInputs = [ 
+      haxe hxcpp
+      tokentree haxeparser hxparse json2object hxargs hxjsonast 
+    ];
+
+    src = fetchFromGitHub {
+      owner = "HaxeCheckstyle";
+      repo = "haxe-formatter";
+      rev = "7e4984fe5a894a4f1178d9d05d81e3ea2bd8546b";
+      hash = "sha256-7h5TEUJ4eKXSheLjxE/Skleb5CRD+eZPtTh+cORLnyU=";
+    };
   };
 }
