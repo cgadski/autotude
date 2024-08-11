380be1a83f2a7739d39c064587417d043c2b34c8
Christopher Gadzinski <me@cgad.ski>
2024-08-11 11:51:14 +0200

    query for jaxx

--- site_src/index.sql
+++ site_src/index.sql
@@ -1,9 +1,12 @@
+WITH good_games AS (
+    SELECT *, count() AS n_goals 
+    FROM replays NATURAL JOIN goals 
+    WHERE name LIKE "%jaxx%" OR name LIKE "%raxx%" 
+    GROUP BY name, replay_id HAVING n_goals >= 5
+)
 SELECT replay_id, time, map, path, ticks,
     JSON_GROUP_ARRAY(JSON_ARRAY(name, team)) AS players
 FROM replays NATURAL JOIN players 
-WHERE map = 'ball_4cliff'
+WHERE replay_id IN (SELECT replay_id FROM good_games)
 GROUP BY replay_id 
-HAVING SUM(name = 'geom') > 0 
-    -- AND SUM(name LIKE '%ken') > 0
-    AND SUM(name LIKE 'Bot %') = 0
 ORDER BY time DESC;
