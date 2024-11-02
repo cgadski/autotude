4cd7672aba9668919afa5e4ee67aac74df839aba
Christopher Gadzinski <me@cgad.ski>
2024-11-02 11:45:46 +0100

    query to count goals in ladder games

---
+++ all_goals.sql
@@ -0,0 +1,24 @@
+WITH
+ladder_games AS (
+    SELECT
+        replay_id,
+        path,
+        COUNT(DISTINCT name) AS n_players,
+        (SELECT COUNT() FROM goals g WHERE g.replay_id == replays.replay_id) AS goals,
+        ticks / (60 * 30) AS minutes
+    FROM replays
+    NATURAL JOIN players
+    WHERE map LIKE "ball_4%" AND minutes >= 2
+    AND NOT EXISTS (SELECT 1
+        FROM players p
+        WHERE p.replay_id == replays.replay_id
+            AND p.name LIKE "Bot%"
+    )
+    GROUP BY replay_id
+    HAVING n_players >= 8 AND goals >= 2
+)
+SELECT name, COUNT() AS goals
+FROM (SELECT replay_id FROM ladder_games)
+NATURAL JOIN goals
+GROUP BY name
+ORDER BY goals DESC
