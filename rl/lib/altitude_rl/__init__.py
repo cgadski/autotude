1893a468e28484c7172579fe38813b23578da1b1
Christopher Gadzinski <cgadski@christophers-macbook-pro-1.home>
2025-04-28 13:02:11 +0200

    can now control online bots

--- rl/lib/altitude_rl/__init__.py
+++ rl/lib/altitude_rl/__init__.py
@@ -1,6 +1,7 @@
-from .proto.command_pb2 import Cmd
+from .proto.command_pb2 import Cmd, ClientCmd
 from .proto.update_pb2 import Update
 from .server_config import ServerConfig
+from .client_config import ClientConfig
 from .bot_server import BotServer
 from .simple_environments import SoloChannelparkEnv
 from .paths import ALTI_HOME, BIN
--- rl/lib/altitude_rl/client_config.py
+++ rl/lib/altitude_rl/client_config.py
@@ -5,11 +5,17 @@ from pathlib import Path
 class ClientConfig:
     def __init__(self, user: str, pw: str, server: str):
         self.root = ET.Element("Config")
-        self.set(mode="PLAY_BOT")
+        self.set(mode="PLAY")
         self.set(record="true")
         self.set(port=27276)
         self.set(accountName=user, accountPassword=pw)
         self.set(server=server, password="")
+        self.set(clearDistances="true")
+
+        self.set(plane="LOOPY")
+        self.set(red=1)
+        self.set(green=1)
+        self.set(blue=1)
 
     def set(self, **kwargs):
         for k, v in kwargs.items():
