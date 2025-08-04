5c73f9f94be951d828f2d8b4bd99ec7050bef07d
3zeah <209227893+3zeah@users.noreply.github.com>
2025-08-04 03:32:04 +0200

    improve definition of ladder game in dump
    this filters false positives: games stopped after 2min, or unranked 4v4 games; and false negatives: ranked games where some player never connects

--- rust_src/schema/views.sql
+++ rust_src/schema/views.sql
@@ -1,27 +1,61 @@
 -- subset of ladder games
 DROP TABLE IF EXISTS ladder_games;
-CREATE TABLE ladder_games AS
-WITH active_players AS (
-    SELECT replay_key, COUNT(DISTINCT vapor) AS ct
-    FROM replays
-    NATURAL JOIN players
-    WHERE team >= 3
-    GROUP BY stem
+CREATE TABLE ladder_games (replay_key INTEGER PRIMARY KEY REFERENCES replays (replay_key));
+INSERT INTO ladder_games (replay_key)
+with chronological_replays as (
+	select
+		*,
+		started_at + duration / 30 as ended_at
+	from replays
+	order by started_at, ended_at, duration
 ),
-n_goals AS (
-    SELECT replay_key, COUNT() AS ct
-    FROM replays
-    NATURAL JOIN goals
-    GROUP BY replay_key
+consecutive_replays as (
+	select
+		*,
+		lead(started_at, 1) over (partition by server) as next_started_at,
+		lead(map, 1) over (partition by server) as next_map,
+		lead(replay_key, 1) over (partition by server) as next_replay_key
+	from chronological_replays
+),
+replays_that_started_ranked as (
+	select DISTINCT replay_key
+	from messages
+	where
+		player_key is null
+		and chat_message = 'Starting ranked game, do not leave if playing.'
+),
+games_that_were_probably_not_stopped as (
+	select DISTINCT replay_key
+	from messages
+	where
+		player_key is null
+		and (
+			chat_message like 'Sudden Death:  First team with a point total of % wins.'
+			or chat_message = 'Game over due to mercy rule.'
+		)
+),
+games_after_restart as (
+	select DISTINCT replay_key
+	from messages
+	where
+		player_key is null
+		and chat_message = 'Game has been restarted.'
 )
-SELECT replay_key FROM replays
-JOIN active_players USING (replay_key)
-JOIN n_goals USING (replay_key)
-WHERE duration > 30 * 120 -- at least two minutes
-AND active_players.ct == 8 -- exactly 8 vapor ids in game
-AND n_goals.ct > 0 -- a goal was scored
-AND stem != 'f6f0ad5e-860a-41bd-af9d-ffe7b30ab41c' -- game started recording late
-AND map != 'lobby_4ball';
+-- a ladder game is a game that transitions from a ranked lobby and actually finishes
+select consecutive_replays.next_replay_key as replay_key
+from replays_that_started_ranked -- preceding game started a ranked game
+inner join consecutive_replays
+	on replays_that_started_ranked.replay_key = consecutive_replays.replay_key
+inner join games_that_were_probably_not_stopped -- succeeeding game was not stopped
+	on games_that_were_probably_not_stopped.replay_key = consecutive_replays.next_replay_key
+where
+	map = 'lobby_4ball' and abs(ended_at - next_started_at) < 15 -- game transitioned from ranked lobby
+	and next_map != 'lobby_4ball' -- and was not played on lobby (starting ranked will never transition back into lobby)
+union
+-- or is a game after a restart that actually finished
+select replay_key
+from games_after_restart
+natural join games_that_were_probably_not_stopped;
 
 -- player display name := manual name (from `names` table), if it exists, or else the most frequent name by replay count
 DROP VIEW IF EXISTS player_display_names;
