bdfc58f685c373f90b9818e1bacab2dbe48cebe0
cgadski <me@cgad.ski>
2025-11-07 16:31:50 +0000

    add handles for poison, curry goat

--- stats_db/misc/players.sql
+++ stats_db/misc/players.sql
@@ -1,20 +1,15 @@
-WITH vapor_idx AS (
-    SELECT vapor, row_number() OVER (ORDER BY vapor) AS idx
-    FROM players
-    WHERE vapor != ''
-    GROUP BY vapor
-),
-games_per_player AS (
+WITH games_per_vapor AS (
     SELECT vapor, COUNT() AS n_games
     FROM ladder_games
     NATURAL JOIN players
     WHERE team >= 3
     GROUP BY vapor
 )
-SELECT idx, vapor, nick, n_games, date(max(started_at), 'unixepoch') AS last_used
+SELECT handle, vapor, nick, n_games, date(max(started_at), 'unixepoch') AS last_used
 FROM players
 NATURAL JOIN replays
-JOIN vapor_idx USING (vapor)
-LEFT JOIN games_per_player USING (vapor)
+NATURAL JOIN player_key_handle
+NATURAL JOIN handles
+LEFT JOIN games_per_vapor USING (vapor)
 GROUP BY vapor, nick
-ORDER BY idx, last_used DESC;
+ORDER BY handle, last_used DESC;
