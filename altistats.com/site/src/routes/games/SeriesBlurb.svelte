e41ba3d954a8b02c634a6136a86932104bde7f5c
cgadski <me@cgad.ski>
2026-05-10 14:21:01 +0200

    update blurb

--- altistats.com/site/src/routes/games/SeriesBlurb.svelte
+++ altistats.com/site/src/routes/games/SeriesBlurb.svelte
@@ -14,15 +14,16 @@
     <p>
         First top dog tournament. 3 round Swiss format group stage followed by
         semi-finals and finals, all games bo3, maps fixed in advance. Teams were
-        organized by players, subject to rating limit.
+        subject to limit on sum of player ratings. Organized by woofy and co,
+        visual design by lemyn.
     </p>
 {/if}
 
 {#if series == 2}
     <p>
-        Second top dog tournament, round robin format, bo5 series, losers could
-        choose order of maps. At start of each bo5, teams could ban a single red
-        perk from the enemy. After each game, losing team could ban 2 more, with
-        the restriction of not banning all 3 red perks for a given plane.
+        Second top dog tournament, round robin format, bo5 series. At start of
+        each bo5, teams could ban a single red perk from the enemy. After each
+        game, losing team could ban 2 more, with the restriction of not banning
+        all 3 red perks for a given plane.
     </p>
 {/if}
