39dfa6bce84bf7598a858479bfa325bcfb9a5856
cgadski <me@cgad.ski>
2026-05-14 19:20:49 +0200

    remove debug query from tables/

--- stats_db/tables/teams.sql
+++
@@ -1,7 +0,0 @@
-SELECT stem, map, datetime(started_at, 'unixepoch'), server, next_key, prev_key
-FROM replays r
-LEFT JOIN replays_wide rw USING (replay_key)
-WHERE (
-    (rw.replay_key is null OR rw.next_key is null)
-    AND server != ''
-)
